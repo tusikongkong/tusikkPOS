d73df2304b02bfe371d44d7968e6ce6dfeaa5563
liuting <liuting33@meituan.com>
2021-03-17 17:10:52 +0800

    fix

---
+++ docs/vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Hello World!',
+    description: 'Just playing around',
+    head: [
+        ['link', { rel: 'icon', href: './public/favicon.png' }],
+    ],
+    themeConfig: {
+        nav: [
+            {test: '概述', link: '/'},
+            {text: '前端'}
+        ]
+    }
+}
